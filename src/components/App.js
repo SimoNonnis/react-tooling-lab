54ad4c7bc00fb844750a2558cc4d7472133633b2
Simon Nonnis <simononnis@gmail.com>
2016-05-19 19:20:33 +0200

    Add ExtractTextPlugin

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import styles from './app.css';
 const App = () => (
   <div>
       <h1 className="title">I love React</h1>
-      <p>A lot really!</p>
+      <p>A lot really</p>
   </div>
 );
 
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,30 +1,34 @@
+var autoprefixer = require('autoprefixer');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 const config = {
- entry: {
-   bundle: './src/main.js'
- },
- output: {
-   filename: './dist/[name].js'
- },
- module: {
-   loaders: [
-     {
-       test: /\.js$/, // ← Test for ".js" file, if it passes, use the loader
-       exclude: /node_modules/,
-       loaders: [
-         'babel' // ← Use babel (short for ‘babel-loader’) loads collection of ES6 transforms, JSX for react etc.. from .babelrc file
-       ]
-     },
-     {
-       test: /\.css$/, // ← Test for ".css" file, if it passes, use the loader
-       loaders: [
-         'style',
-         'css?modules&localIdentName=[name]__[local]____[hash:base64:5]',
-         'postcss'
-       ] // ← loaders working bottom to top
-     }
-   ]
- },
- postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ]
+  entry: {
+    bundle: './src/main.js'
+  },
+  output: {
+    filename: './dist/[name].js'
+  },
+  module: {
+    loaders: [
+      {
+      test: /\.js$/, // ← Test for ".js" file, if it passes, use the loader
+      exclude: /node_modules/,
+      loaders: [
+        'babel' // ← Use babel (short for ‘babel-loader’) loads collection ofES6 transforms, JSX for react etc.. from .babelrc file
+      ]
+      },
+      {
+        test: /\.css$/, // ← Test for ".css" file, if it passes, use the loader
+        loader: ExtractTextPlugin.extract(
+          'css?modules&localIdentName=[name]__[local]____[hash:base64:5]!postcss' // ← loaders working right to left. ExtractTextPlugin requires use of ! syntax.
+        )
+      }
+    ]
+  },
+  postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ],
+  plugins: [
+    new ExtractTextPlugin('./dist/main.min.css')
+  ]
 };
 
 module.exports = config;
