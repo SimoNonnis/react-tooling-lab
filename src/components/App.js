10082a131fbaa3586804aebadd2942fd2bf99cac
Simon Nonnis <simononnis@gmail.com>
2016-10-04 21:07:17 +0100

    'Add react-css-modules'

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import CSSModules from 'react-css-modules';
 import styles from './app.css';
 
 
 const App = () => (
   <div>
-      <h1 className={styles.title}>React + Webpack Boilerplate</h1>
+      <h1 className='global-css' styleName='title bar'>React + Webpack Boilerplate</h1>
       <p>Let's start coding</p>
   </div>
 );
 
-export default App;
+
+
+export default CSSModules(App, styles, {allowMultiple: true});
